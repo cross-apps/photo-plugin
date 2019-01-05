96734d8ff0d851c847749f196c84077e1bcac0a9
Tahsin Yurtseven <me@cross-apps.com>
2019-01-05 04:55:46 +0100

    nuget project for ios restructured

--- CrossAppsPhotoPlugin.iOS/Renderer/MainPageRenderer.cs
+++ CrossAppsPhotoPlugin.iOS/Renderer/MainPageRenderer.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using PhotoTaker;
-using PhotoTaker.iOS.Renderer;
+using CrossAppsPhotoPlugin;
+using CrossAppsPhotoPlugin.iOS.Renderer;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(MainPage), typeof(MainPageRenderer))]
-namespace PhotoTaker.iOS.Renderer
+// [assembly: ExportRenderer(typeof(MainPage), typeof(MainPageRenderer))]
+namespace CrossAppsPhotoPlugin.iOS.Renderer
 {
     public class MainPageRenderer : PageRenderer
     {
